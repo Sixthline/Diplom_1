56e0af0f04b34a6e8390c71684dfdefa8b05493c
Alexey Sharov <sixthline25@gmail.com>
2023-05-08 21:24:10 +0300

    Добавлены тесты

---
+++ src/test/java/IngredientTypeTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTypeTest {
+    @Test
+    public void valueOfIngredientTypeTest() {
+        assertEquals(IngredientType.SAUCE, IngredientType.valueOf("SAUCE"));
+        assertEquals(IngredientType.FILLING, IngredientType.valueOf("FILLING"));
+    }
+}
